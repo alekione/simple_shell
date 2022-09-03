4198adb36447c45ac89550aad4df5efdd63a34f4
lexa9448 <lexa9448@gmail.com>
2022-09-03 11:36:46 +0100

    add new prototypes and modify the previous ones

--- main.h
+++ main.h
@@ -9,23 +9,34 @@
 #include <stdbool.h>
 #include <sys/wait.h>
 #include <signal.h>
+#include <fcntl.h>
+#include <errno.h>
 
+void isexit(char *[]);
 void sig_handler_child(int);
 void sig_handler_parent(int);
 char *strjn(char *, char*);
-void interactive(char *[]);
+void interactive(void);
+void interactive2( char *[], char *);
 bool pathexist(char *);
 char *stripstr(char *);
 void createargv(int, char *[], char *, char [], char);
 int processenv(char *[]);
-int cd (char *[]);
+int cd (char *);
 bool isreadable(char *);
 bool iswritable(char *);
 bool isexecutable(char *);
 char *iscommand(char *, char *);
-void process_multiple(char *[], char *[]);
-int complete_process_multiple(char *[], char *[], char *);
-int process_other(char *[], char *[]);
+int process_file(char *);
+int process_file2(char *[]);
+void process_multiple(char *[]);
+int complete_process_multiple(char *[], char *);
+int process_other(char *[]);
 bool ismore_than_onecommand(char *[]);
-int execute_command(char *[], char *[]);
+int execute_command(char *[]);
+void process_dollar_sign(char *[]);
+char *string_dollar(char *);
+char *num_tostring(unsigned long int);
+char *strchar(char);
+int string_dollar2(int, int, char *, char **);
 #endif
